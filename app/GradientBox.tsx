3fc9360183c454ce189b8ce13d4d7f43c1725812
Matteo Gamba <matteo@foronered.com>
2023-10-20 20:52:33 +0200

    extract styles into constants

--- app/GradientBox.tsx
+++ app/GradientBox.tsx
@@ -11,6 +11,52 @@ const style: React.CSSProperties = {
   padding: 40,
 };
 
+const textLeft: React.CSSProperties = {
+  position: "absolute",
+  alignSelf: "center",
+  left: -120 - 40,
+  top: 0,
+  bottom: 0,
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  width: 120,
+  fontSize: 12,
+  textAlign: "right",
+  color: "rgba(203, 203, 203, 1)",
+};
+
+const textMiddle: React.CSSProperties = {
+  position: "absolute",
+  alignSelf: "center",
+  left: 0,
+  right: 0,
+  margin: "auto",
+  top: -64,
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  width: 120,
+  fontSize: 12,
+  textAlign: "center",
+  color: "rgba(203, 203, 203, 1)",
+};
+
+const textRight: React.CSSProperties = {
+  position: "absolute",
+  alignSelf: "center",
+  right: -144 - 40,
+  top: 0,
+  bottom: 0,
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  width: 144,
+  fontSize: 12,
+  textAlign: "left",
+  color: "rgba(203, 203, 203, 1)",
+};
+
 export const GradientBox: React.FC<{ children: ReactNode }> = (props) => {
   return (
     <div style={style}>
@@ -20,61 +66,9 @@ export const GradientBox: React.FC<{ children: ReactNode }> = (props) => {
         alt="decoration around the gradient box"
         style={{ width: 852, position: "absolute", top: -60, left: -48 }}
       />
-      <div
-        style={{
-          position: "absolute",
-          alignSelf: "center",
-          left: -120 - 40,
-          top: 0,
-          bottom: 0,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          width: 120,
-          fontSize: 12,
-          textAlign: "right",
-          color: "rgba(203, 203, 203, 1)",
-        }}
-      >
-        A personalized video made just for you
-      </div>
-      <div
-        style={{
-          position: "absolute",
-          alignSelf: "center",
-          left: 0,
-          right: 0,
-          margin: "auto",
-          top: -64,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          width: 120,
-          fontSize: 12,
-          textAlign: "center",
-          color: "rgba(203, 203, 203, 1)",
-        }}
-      >
-        Your favorite Github statistics, animated
-      </div>
-      <div
-        style={{
-          position: "absolute",
-          alignSelf: "center",
-          right: -144 - 40,
-          top: 0,
-          bottom: 0,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          width: 144,
-          fontSize: 12,
-          textAlign: "left",
-          color: "rgba(203, 203, 203, 1)",
-        }}
-      >
-        An MP4 video to share with your community
-      </div>
+      <div style={textLeft}>A personalized video made just for you</div>
+      <div style={textMiddle}>Your favorite Github statistics, animated</div>
+      <div style={textRight}>An MP4 video to share with your community</div>
       {props.children}
     </div>
   );
