990a7d265dd4677ad7bcca6e153de32a88715701
Jonny Burger <jonathanburger11@gmail.com>
2023-11-02 14:24:01 +0100

    make a folder that optimizes SVGs

---
+++ optimize-svgbin.ts
@@ -0,0 +1,13 @@
+// bun optimize-all.ts
+
+import { readdirSync, readFileSync, writeFileSync } from "fs";
+import { optimize } from "svgo";
+
+const files = readdirSync("svgbin").filter((f) => f.endsWith(".svg"));
+
+for (const file of files) {
+  console.log(`Optimizing ${file}...`);
+  const filename = `svgbin/${file}`;
+  const opt = optimize(readFileSync(filename, "utf-8"));
+  writeFileSync(filename, opt.data);
+}
