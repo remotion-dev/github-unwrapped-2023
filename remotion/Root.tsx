4329dae92c516f3e8e933ee06d910b5004c5e4c3
Jonny Burger <jonathanburger11@gmail.com>
2023-11-02 10:17:27 +0100

    add more compositions

--- remotion/Root.tsx
+++ remotion/Root.tsx
@@ -5,7 +5,9 @@ import {
   VIDEO_HEIGHT,
   VIDEO_WIDTH,
 } from "../types/constants";
+import { Issues } from "./Issues";
 import { Spaceship } from "./Spaceship";
+import { TopLanguages } from "./TopLanguages";
 
 export const RemotionRoot: React.FC = () => {
   return (
@@ -13,6 +15,24 @@ export const RemotionRoot: React.FC = () => {
       <Composition
         id={"Spaceship"}
         component={Spaceship}
+        durationInFrames={12 * 30}
+        fps={VIDEO_FPS}
+        width={VIDEO_WIDTH}
+        height={VIDEO_HEIGHT}
+        defaultProps={defaultMyCompProps}
+      />
+      <Composition
+        id={"Issues"}
+        component={Issues}
+        durationInFrames={12 * 30}
+        fps={VIDEO_FPS}
+        width={VIDEO_WIDTH}
+        height={VIDEO_HEIGHT}
+        defaultProps={defaultMyCompProps}
+      />
+      <Composition
+        id={"TopLanguages"}
+        component={TopLanguages}
         durationInFrames={12 * 30}
         fps={VIDEO_FPS}
         width={VIDEO_WIDTH}
