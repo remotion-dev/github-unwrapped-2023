78b34de4bad8b097bae32dc44a1028d09af35f13
Matteo Gamba <matteo@foronered.com>
2023-11-02 18:08:55 +0100

    add icons

---
+++ icons/LinkedInIcon.tsx
@@ -0,0 +1,35 @@
+export const LinkedInIcon: React.FC<React.SVGProps<SVGSVGElement>> = (
+  props
+) => (
+  <svg
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <g clip-path="url(#clip0_359_380)">
+      <path
+        d="M8.99112 6.99925C10.6687 4.24849 15.9251 4.59022 15.9251 9.11064V15.921H12.7476V9.95872C12.7476 8.98129 11.9565 8.18906 10.9805 8.18906C10.0045 8.18906 9.21347 8.98129 9.21347 9.95872V15.921H5.97656V5.57132H8.98672L8.99039 6.99852L8.99112 6.99925Z"
+        fill="currentColor"
+      />
+      <path
+        d="M3.94098 5.5625H0.71875V15.9262H3.94098V5.5625Z"
+        fill="currentColor"
+      />
+      <path
+        d="M4.1958 2.52225C4.2979 1.48865 3.54401 0.56786 2.51193 0.465605C1.47986 0.363349 0.560421 1.11835 0.458316 2.15195C0.35621 3.18554 1.1101 4.10633 2.14218 4.20859C3.17425 4.31084 4.09369 3.55584 4.1958 2.52225Z"
+        fill="currentColor"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_359_380">
+        <rect
+          width="15.4713"
+          height="15.4713"
+          fill="currentColor"
+          transform="translate(0.453125 0.453125)"
+        />
+      </clipPath>
+    </defs>
+  </svg>
+);
