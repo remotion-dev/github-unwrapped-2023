04f91c0634e7d507da41573dcde8c353b09b1458
Jonny Burger <jonathanburger11@gmail.com>
2023-11-17 15:26:50 +0100

    add back CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "../components/Home";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Home />
   </React.StrictMode>
 );
-
-import.meta.env.BASE_URL;
