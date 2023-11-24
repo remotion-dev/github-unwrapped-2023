cc5fc95ca1bc56099e42064f61b6db7d7d2e1ff5
Matteo Gamba <matteo@foronered.com>
2023-11-24 15:47:22 +0100

    first version of transformed graph in tablet

---
+++ remotion/Productivity/Tablet.tsx
@@ -0,0 +1,55 @@
+import type { ComponentProps } from "react";
+import { AbsoluteFill } from "remotion";
+import { ProductivityGraph, type Productivity } from "./Productivity";
+import { TabletSVG } from "./TabletSVG";
+
+export const Tablet: React.FC<ComponentProps<typeof Productivity>> = ({
+  graphData,
+}) => {
+  return (
+    <AbsoluteFill
+      style={{
+        display: "flex",
+      }}
+    >
+      <TabletSVG
+        style={{ width: "100%", height: "100%", position: "relative" }}
+      />
+      {/* <div
+        style={{
+          width: 600,
+          aspectRatio: 3 / 2,
+          background: " red ",
+          left: 400,
+          top: 280,
+          position: "absolute",
+          transform: "perspective(1500px) rotateY(15deg) skew(6deg,-3deg)",
+        }}
+      /> */}
+      <div
+        style={{
+          left: 220,
+          top: 160,
+          position: "absolute",
+          display: "flex",
+          flexDirection: "column",
+          gap: 24,
+          transform:
+            "perspective(1500px) rotateY(15deg) skew(6deg,-3deg) scale(0.6)",
+        }}
+      >
+        <div
+          style={{
+            color: "white",
+            fontFamily: "Mona Sans",
+            fontWeight: 800,
+            fontSize: 80,
+          }}
+        >
+          Monday
+        </div>
+        <ProductivityGraph productivityPerHour={graphData} />
+      </div>
+    </AbsoluteFill>
+  );
+};
--- remotion/Root.tsx
+++ remotion/Root.tsx
@@ -24,6 +24,7 @@ import { PullRequests } from "./Paths/Paths";
 import { WholePaths } from "./Paths/WholePaths";
 import { Poof } from "./Poof";
 import { Productivity } from "./Productivity/Productivity";
+import { Tablet } from "./Productivity/Tablet";
 import {
   SevenSegment,
   sevenSegmentSchema,
@@ -192,6 +193,42 @@ export const RemotionRoot: React.FC = () => {
       <Composition
         id={"Productivity"}
         component={Productivity}
+        durationInFrames={10 * VIDEO_FPS}
+        fps={VIDEO_FPS}
+        width={VIDEO_WIDTH}
+        height={VIDEO_HEIGHT}
+        defaultProps={{
+          graphData: [
+            { time: 4, productivity: 0 },
+            { time: 5, productivity: 0.1 },
+            { time: 6, productivity: 0.15 },
+            { time: 7, productivity: 0.3 },
+            { time: 8, productivity: 0.35 },
+            { time: 9, productivity: 0.3 },
+            { time: 10, productivity: 0.25 },
+            { time: 11, productivity: 0.3 },
+            { time: 12, productivity: 0.2 },
+            { time: 13, productivity: 0.5 },
+            { time: 14, productivity: 0.6 },
+            { time: 15, productivity: 0.7 },
+            { time: 16, productivity: 0.5 },
+            { time: 17, productivity: 0.5 },
+            { time: 18, productivity: 0.4 },
+            { time: 19, productivity: 0.2 },
+            { time: 20, productivity: 0.1 },
+            { time: 21, productivity: 0.3 },
+            { time: 22, productivity: 0.2 },
+            { time: 23, productivity: 0.1 },
+            { time: 24, productivity: 0.05 },
+            { time: 1, productivity: 0 },
+            { time: 2, productivity: 0 },
+            { time: 3, productivity: 0 },
+          ],
+        }}
+      />
+      <Composition
+        id={"Tablet"}
+        component={Tablet}
         durationInFrames={10 * VIDEO_FPS}
         fps={VIDEO_FPS}
         width={VIDEO_WIDTH}
