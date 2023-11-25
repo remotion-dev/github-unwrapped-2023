5ad8686b549aed3ff2ac373365a00a26f352360a
Jonny Burger <jonathanburger11@gmail.com>
2023-11-25 10:20:49 +0100

    gradients go to transparent

--- remotion/Gradients/available-gradients.ts
+++ remotion/Gradients/available-gradients.ts
@@ -1,8 +1,8 @@
 export const availableGradients = {
-  orange: "radial-gradient(#DD8B5A, #000000 70%)",
-  blue: "radial-gradient(#32588D, #000000 70%)",
-  yellow: "radial-gradient(#E7D541, #000000 70%)",
-  brown: "radial-gradient(#3E3429, #000000 70%)",
+  orange: "radial-gradient(#DD8B5A, rgba(0, 0, 0, 0) 70%)",
+  blue: "radial-gradient(#32588D, rgba(0, 0, 0, 0) 70%)",
+  yellow: "radial-gradient(#E7D541, rgba(0, 0, 0, 0) 70%)",
+  brown: "radial-gradient(#3E3429, rgba(0, 0, 0, 0) 70%)",
 };
 
 export type GradientType = keyof typeof availableGradients;
