31dbbfafb7f7547669a0741e901e029d81fb73c5
Jonny Burger <jonathanburger11@gmail.com>
2023-11-26 14:34:57 +0100

    Update IssueNumber.tsx

--- remotion/Issues/IssueNumber.tsx
+++ remotion/Issues/IssueNumber.tsx
@@ -7,6 +7,7 @@ import {
   useVideoConfig,
 } from "remotion";
 import { SevenSegment } from "../SevenSegment/SevenSegmentNumber";
+import { PANE_BACKGROUND, PANE_BORDER } from "../TopLanguages/Pane";
 
 export const IssueNumber: React.FC<{
   totalIssues: number;
@@ -32,29 +33,45 @@ export const IssueNumber: React.FC<{
         color: "white",
         fontFamily: "Mona Sans",
         fontWeight: "800",
-        justifyContent: "flex-end",
-        alignItems: align === "left" ? "flex-start" : "flex-end",
-        padding: 40,
       }}
     >
-      <div style={{ opacity }}>
-        <SevenSegment
-          max={totalIssues}
-          num={Math.round(currentNumber * totalIssues)}
-          fontSize={120}
-        />
-      </div>
       <div
         style={{
-          fontFamily: "Mona Sans",
-          color: "white",
-          fontSize: 30,
-          marginBottom: 14,
-          paddingLeft: align === "left" ? 15 : 0,
-          paddingRight: align === "right" ? 9 : 0,
+          background: PANE_BACKGROUND,
+          border: PANE_BORDER,
+          borderRadius: 9,
+          alignItems: align === "left" ? "flex-start" : "flex-end",
+          display: "inline-flex",
+          flexDirection: "column",
+          [align === "left" ? "left" : "right"]: 40,
+          width: 300,
+          position: "absolute",
+          bottom: 40,
+          paddingLeft: 10,
+          paddingRight: 10,
+          paddingTop: 5,
+          paddingBottom: 5,
         }}
       >
-        {label}
+        <div style={{ opacity }}>
+          <SevenSegment
+            max={totalIssues}
+            num={Math.round(currentNumber * totalIssues)}
+            fontSize={120}
+          />
+        </div>
+        <div
+          style={{
+            fontFamily: "Mona Sans",
+            color: "white",
+            fontSize: 30,
+            marginBottom: 14,
+            paddingLeft: align === "left" ? 15 : 0,
+            paddingRight: align === "right" ? 9 : 0,
+          }}
+        >
+          {label}
+        </div>
       </div>
     </AbsoluteFill>
   );
