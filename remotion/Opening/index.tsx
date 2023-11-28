1289cdd688810cdcd55a41b205a43ddab4aa92a6
Luke Zirngibl <luke.zirngibl@bob.ch>
2023-11-28 15:56:53 +0100

    adding assets

---
+++ remotion/Opening/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { AbsoluteFill, useCurrentFrame } from "remotion";
+import Background from "./Background";
+import Foreground from "./Foreground";
+import Spaceship from "./Spaceship";
+
+export const OpeningScene: React.FC = () => {
+  // const { fps, durationInFrames, width, height } = useVideoConfig();
+  const frame = useCurrentFrame();
+
+  return (
+    <AbsoluteFill
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <AbsoluteFill
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          fontSize: 60,
+          width: "100%",
+          height: "100%",
+          background:
+            "radial-gradient(121.11% 121.11% at 47.08% 100%, #0F102E 0%, #000 100%)",
+        }}
+      >
+        <AbsoluteFill>
+          <Background />
+        </AbsoluteFill>
+
+        <AbsoluteFill style={{ top: -100 }}>
+          <Spaceship />
+        </AbsoluteFill>
+
+        <AbsoluteFill style={{ top: 0 }}>
+          <Foreground />
+        </AbsoluteFill>
+      </AbsoluteFill>
+    </AbsoluteFill>
+  );
+};
