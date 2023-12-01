d7848025e3e943aa1c97c8c3651cdd1c311d9be6
Matteo Gamba <matteo@foronered.com>
2023-12-01 11:13:52 +0100

    fix input  not clickable

--- vite/HomeBox/HomeBox.tsx
+++ vite/HomeBox/HomeBox.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Box } from "../Box/Box";
-import { Octocat } from "../Octocat";
 import { HomeBoxBottom } from "./HomeBoxBottom";
 import { HomeBoxTop } from "./HomeBoxTop";
 import styles from "./styles.module.css";
@@ -9,15 +8,14 @@ export const HomeBox: React.FC = () => {
   const [userNotFound, setUserNotFound] = useState<boolean>(false);
 
   return (
-    <>
-      <Box className={styles.homeBoxWrapper} style={{ maxWidth: 800 }}>
-        <HomeBoxTop />
-        <HomeBoxBottom
-          setUserNotFound={setUserNotFound}
-          userNotFound={userNotFound}
-        />
-      </Box>
-      <Octocat userNotFound={userNotFound} />
-    </>
+    <Box className={styles.homeBoxWrapper} style={{ maxWidth: 800, zIndex: 1 }}>
+      <HomeBoxTop />
+      <HomeBoxBottom
+        setUserNotFound={setUserNotFound}
+        userNotFound={userNotFound}
+      />
+    </Box>
   );
 };
+
+/* <Octocat userNotFound={userNotFound} /> */
