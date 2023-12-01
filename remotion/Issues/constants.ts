1bfb370ba12e3dd24b17a6ec96b954ad7ba2c028
Jonny Burger <jonathanburger11@gmail.com>
2023-12-01 17:18:28 +0100

    new rocket

--- remotion/Issues/constants.ts
+++ remotion/Issues/constants.ts
@@ -1,11 +1,10 @@
 import { VIDEO_WIDTH } from "../../types/constants";
+import { TL_ROCKET_HEIGHT } from "../TopLanguages/svgs/NewRocketSVG";
 
-export const ROCKET_HEIGHT = 245;
 export const PADDING = 120;
 export const USABLE_CANVAS_WIDTH = VIDEO_WIDTH - PADDING * 2;
 export const ROCKET_ORIGIN_X = VIDEO_WIDTH / 2;
 const ROCKET_ORIGIN_Y = VIDEO_WIDTH - 150;
-export const ROCKET_TOP_Y = ROCKET_ORIGIN_Y - ROCKET_HEIGHT / 2;
+export const ROCKET_TOP_Y = ROCKET_ORIGIN_Y - TL_ROCKET_HEIGHT / 2;
 export const TIME_BEFORE_SHOOTING = 70;
 export const TOTAL_SHOOT_DURATION = 60;
-export const ROCKET_WIDTH = 138;
