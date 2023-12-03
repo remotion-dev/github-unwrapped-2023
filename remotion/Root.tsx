75f90c27019d1b32b9c9b620f6e185cb9a6ba068
Jonny Burger <jonathanburger11@gmail.com>
2023-12-03 17:43:00 +0100

    better test case for productivity

--- remotion/Root.tsx
+++ remotion/Root.tsx
@@ -372,6 +372,7 @@ export const RemotionRoot: React.FC = () => {
           width={1080}
           defaultProps={{
             extraPaths: 8,
+            initialPullRequests: 0,
           }}
         />
         <Composition
@@ -538,7 +539,104 @@ export const RemotionRoot: React.FC = () => {
           totalPullRequests: 10,
           topWeekday: "2",
           topHour: "0",
-          graphData: GRAPH_DATA,
+          graphData: [
+            {
+              productivity: 0,
+              time: 0,
+            },
+            {
+              productivity: 0,
+              time: 1,
+            },
+            {
+              productivity: 0,
+              time: 2,
+            },
+            {
+              productivity: 0,
+              time: 3,
+            },
+            {
+              productivity: 0,
+              time: 4,
+            },
+            {
+              productivity: 0,
+              time: 5,
+            },
+            {
+              productivity: 0,
+              time: 6,
+            },
+            {
+              productivity: 5,
+              time: 7,
+            },
+            {
+              productivity: 29,
+              time: 8,
+            },
+            {
+              productivity: 49,
+              time: 9,
+            },
+            {
+              productivity: 58,
+              time: 10,
+            },
+            {
+              productivity: 49,
+              time: 11,
+            },
+            {
+              productivity: 17,
+              time: 12,
+            },
+            {
+              productivity: 48,
+              time: 13,
+            },
+            {
+              productivity: 43,
+              time: 14,
+            },
+            {
+              productivity: 54,
+              time: 15,
+            },
+            {
+              productivity: 33,
+              time: 16,
+            },
+            {
+              productivity: 52,
+              time: 17,
+            },
+            {
+              productivity: 35,
+              time: 18,
+            },
+            {
+              productivity: 12,
+              time: 19,
+            },
+            {
+              productivity: 9,
+              time: 20,
+            },
+            {
+              productivity: 5,
+              time: 21,
+            },
+            {
+              productivity: 2,
+              time: 22,
+            },
+            {
+              productivity: 0,
+              time: 23,
+            },
+          ],
         }}
       />
       <Composition
