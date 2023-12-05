896368a4b9c2b2434f86ea68335a82ea6caa6ba1
Jonny Burger <jonathanburger11@gmail.com>
2023-12-05 17:12:02 +0100

    Update get-audio-hits.ts

--- remotion/Issues/get-audio-hits.ts
+++ remotion/Issues/get-audio-hits.ts
@@ -6,7 +6,7 @@ import type {
 import { sampleUniqueIndices } from "./sample-indices";
 
 const MAX_SHOTS = 5;
-const MAX_HITS = 5;
+const MAX_HITS = 4;
 
 if (MAX_SHOTS + MAX_HITS > MAXIMUM_NUMBER_OF_AUDIO_TAGS - 1) {
   throw new Error(
@@ -43,7 +43,7 @@ export const getAudioHits = (
     ...explosionSamples.map((index): AudioSample => {
       return {
         type: "explosion" as const,
-        delay: explosions[index].explodeAfterFrames,
+        delay: explosions[index].explodeAfterFrames - 2,
       };
     }),
   ];
