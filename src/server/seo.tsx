469c231cd3c364519276e3d32378f8180a9856d9
Jonny Burger <jonathanburger11@gmail.com>
2023-12-06 13:35:39 +0100

    SEO tags

--- src/server/seo.tsx
+++ src/server/seo.tsx
@@ -3,11 +3,30 @@ import React from "react";
 import { renderToString } from "react-dom/server";
 import { getStatsFromGitHubOrCache } from "./fetch-stats.js";
 import { getRandomGithubToken } from "./github-token.js";
+import { backendCredentials } from "../helpers/domain.js";
 
 const makeAppHead = async (username: string | null) => {
   if (username === null) {
-    const title = `#GitHubUnwrapped 2023`;
-    return renderToString(<title>{title}</title>);
+    const title = `#GitHubUnwrapped 2023 - Your coding year in review`;
+
+    const socialPreview = `${backendCredentials().VITE_HOST}/og_image.png`;
+    const canonical = `${backendCredentials().VITE_HOST}`;
+    const description =
+      "Get your personalized video of your GitHub activity in 2023.";
+
+    return renderToString(
+      <>
+        <title>{title}</title>
+        <meta property="og:url" content={canonical} />
+        <meta property="og:image" content={socialPreview} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={socialPreview} />
+        <meta name="description" content={description} />
+        <link rel="canonical" href={canonical} />
+      </>,
+    );
   }
 
   const stats = await getStatsFromGitHubOrCache({
