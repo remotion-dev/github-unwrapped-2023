36f57db6bae2f016b979a51bc7a74c90cd908b5d
Jonny Burger <jonathanburger11@gmail.com>
2023-12-07 17:22:24 +0100

    Update available-gradients.ts

--- remotion/Gradients/available-gradients.ts
+++ remotion/Gradients/available-gradients.ts
@@ -6,9 +6,9 @@ export const availableGradients = {
   brown: "radial-gradient(#3E3429, rgba(0, 0, 0, 0) 70%)",
   white: "radial-gradient(#FFFFFF, rgba(0, 0, 0, 0) 70%)",
   blueRadial:
-    "radial-gradient(100% 100% at 47.08% 100%, #0F102E 0%, #000 100%)",
+    "radial-gradient(100% 100% at 47.08% 100%, #0F102E 0%, rgba(0, 0, 0, 0) 100%)",
   purpleRadial:
-    "radial-gradient(100% 100% at 47.08% 100%, #381945 0%, #000 100%)",
+    "radial-gradient(100% 100% at 47.08% 100%, #381945 0%, rgba(0, 0, 0, 0) 100%)",
 };
 
 export type GradientType = keyof typeof availableGradients;
