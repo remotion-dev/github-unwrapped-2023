d55c56558bf5b9ae0e431a8e95ed647d721d14b8
Luke Zirngibl <luke.zirngibl@bob.ch>
2023-12-11 16:07:59 +0100

    fix loading

--- vite/Loading.tsx
+++ vite/Loading.tsx
@@ -1,8 +1,6 @@
 // import { HomeForeground } from "./Home/HomeForeground";
 // import { Navbar } from "./Home/Navbar";
 // import { Planet } from "./Home/Planet";
-import { useEffect } from "react";
-import { Stars } from "./Home/Stars";
 // import { HomeBox } from "./HomeBox/HomeBox";
 import { RadialGradient } from "./RadialGradient";
 import styles from "./styles.module.css";
@@ -10,31 +8,31 @@ import styles from "./styles.module.css";
 export const Loading = () => {
   const username = window.location.pathname.split("/")[2];
 
-  useEffect(() => {
-    fetch("/api/stats", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        username,
-      }),
-    })
-      // TODO
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      .then((res) => {
-        window.location.href = `/${username}`;
-      })
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      .catch((err) => {
-        // TODO
-      });
-  }, [username]);
+  // useEffect(() => {
+  //   fetch("/api/stats", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       username,
+  //     }),
+  //   })
+  //     // TODO
+  //     // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  //     .then((res) => {
+  //       // window.location.href = `/${username}`;
+  //     })
+  //     // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  //     .catch((err) => {
+  //       // TODO
+  //     });
+  // }, [username]);
 
   return (
     <div className={styles.container}>
       <RadialGradient />
-      <Stars />
+      {/* <Stars /> */}
       <div
         style={{
           height: "100vh",
@@ -55,7 +53,22 @@ export const Loading = () => {
           src="/walking-octocat.gif"
           alt="loading"
         />
-        <p style={{ width: 144, textAlign: "center" }}>Unwrapping...</p>
+        <div
+          style={{
+            width: 144,
+            height: 6,
+            borderRadius: 6,
+            overflow: "hidden",
+            background: "rgba(255,255,255,0.1)",
+            marginTop: 48,
+          }}
+        >
+          <div
+            className={styles.animateProgress}
+            style={{ height: 12, background: "white" }}
+          ></div>
+        </div>
+        {/* <p style={{ width: 144, textAlign: "center" }}>Unwrapping...</p> */}
       </div>
     </div>
   );
