48aabb841941a5fc06d1ab34080ec1658d181884
MehmetAdemi <mehmet@remotion.dev>
2023-12-11 14:38:17 +0100

    Update SharingActions.tsx

--- vite/VideoPage/Actions/SharingActions.tsx
+++ vite/VideoPage/Actions/SharingActions.tsx
@@ -7,7 +7,7 @@ import { SharingAction } from "./SharingAction";
 import styles from "./styles.module.css";
 
 export const twitterSharingLink = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
-  "This is my #GitHubUnwrapped! Get your own: https://www.githubunwrapped.com\n\n[Delete this placeholder, download and drag your MP4 video in here]",
+  "This is my #GitHubUnwrapped! Get your own: https://www.githubunwrapped.com\n\n[DELETE THIS PLACEHOLDER, DOWNLOAD AND DRAG YOUR MP4 VIDEO IN HERE]",
 )}`;
 
 export const linkedInSharingLink = "https://www.linkedin.com/";
