5599284ab9e3f39475e9d9be7cff9c92ffdc2207
MehmetAdemi <mehmet@remotion.dev>
2023-12-12 09:03:50 +0100

    Update components.tsx

--- vite/About/components.tsx
+++ vite/About/components.tsx
@@ -1,11 +1,12 @@
 import { Link } from "@tanstack/react-router";
 import type { ComponentProps } from "react";
+import styles from "./styles.module.css";
 
 export const NavigateBack: React.FC<{
   backLink?: ComponentProps<typeof Link>;
-}> = () => {
+}> = ({ backLink }) => {
   return (
-    <Link to={"/"}>
+    <Link to={"/"} className={styles.navigateBack} {...backLink}>
       <img
         src="/arrow.svg"
         style={{ width: 32, height: 32, transform: "rotate(180deg)" }}
