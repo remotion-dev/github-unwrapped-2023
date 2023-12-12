5dd0805c706f2a20af4d1b47e26c661d5cca7758
MehmetAdemi <mehmet@remotion.dev>
2023-12-12 08:58:31 +0100

    Fix backLink for arrow on About page

--- vite/About/components.tsx
+++ vite/About/components.tsx
@@ -1,12 +1,11 @@
 import { Link } from "@tanstack/react-router";
 import type { ComponentProps } from "react";
-import styles from "./styles.module.css";
 
 export const NavigateBack: React.FC<{
   backLink?: ComponentProps<typeof Link>;
-}> = ({ backLink }) => {
+}> = () => {
   return (
-    <Link className={styles.navigateBack} {...backLink}>
+    <Link to={"/"}>
       <img
         src="/arrow.svg"
         style={{ width: 32, height: 32, transform: "rotate(180deg)" }}
