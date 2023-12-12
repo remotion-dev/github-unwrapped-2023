95b8b0fbd24d0b94c7d2425d134d430fd187c280
Matteo Gamba <matteo@foronered.com>
2023-12-12 12:12:32 +0100

    remove back button on headers

--- vite/About/MobileHeader.tsx
+++ vite/About/MobileHeader.tsx
@@ -1,16 +1,13 @@
-import type { ComponentProps } from "react";
 import { Navbar } from "../Home/Navbar";
-import { NavigateBack } from "./components";
 import styles from "./styles.module.css";
 
 export const MobileHeader: React.FC<{
   title: string;
   description?: string;
-  backLink?: ComponentProps<typeof NavigateBack>["backLink"];
-}> = ({ title, description, backLink }) => {
+}> = ({ title, description }) => {
   return (
     <div className={styles.mobileHeaderWrapper}>
-      <Navbar>{backLink && <NavigateBack backLink={backLink} />}</Navbar>
+      <Navbar />
       <div className={styles.mobileHeaderContent}>
         <h1 className={styles.aboutTitle}>{title}</h1>
         {description && (
